17ed949b868eaa55e5eb0a6c27c8dafa8ac48910
mtbishop <bishopmatthew76@gmail.com>
2022-01-07 12:26:24 -0700

    linked wiki of people in space to their names

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./styles/App.scss";
-import { Route, Routes } from "react-router-dom"
-import HomePage from './pages/HomePage'
+import { Route, Routes } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import PeopleInSpace from './pages/PeopleInSpace';
+import Tracker from './pages/Tracker';
 
 function App() {
   return (
   <div className="container">
     <Routes>
       <Route path='/' element={<HomePage />}/>
+      <Route path='/people' element={<PeopleInSpace />}/>
+      <Route path='/tracker' element={<Tracker />}/>
     </Routes>
   </div>
   );
--- src/pages/PeopleInSpace.jsx
+++ src/pages/PeopleInSpace.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
+
+
 
 const PeopleInSpace = () => {
-  return <div>PEOPLE IN SPACE</div>;
+
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    const fetchPeople = async() => {
+      const response = await axios.get('http://api.open-notify.org/astros.json')
+      console.log("Data: ", response.data);
+      setPeople(response.data.people)
+    }
+    fetchPeople();
+  },[])
+
+  return (
+  <div className="container">
+    <div className="astronauts"></div>
+    <div className="peopleInSpace">
+      {people.map((person, idx) => (
+        <a key={person.name} className="names" href={`https://en.wikipedia.org/wiki/${person.name}`}
+        target={"_blank"} rel="noreferrer">{person.name}</a>
+      ))}
+    </div>
+  </div>
+  )
 };
 
 export default PeopleInSpace;
