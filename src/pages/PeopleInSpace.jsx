00b4c2f7d81a6d21980c38bc3d47d2fc8d2980d1
mtbishop <bishopmatthew76@gmail.com>
2022-01-07 12:51:10 -0700

    error catching with loading

--- src/pages/PeopleInSpace.jsx
+++ src/pages/PeopleInSpace.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import SyncLoader from "react-spinners/SyncLoader"
 import axios from 'axios'
 
 
@@ -7,12 +8,19 @@ import axios from 'axios'
 const PeopleInSpace = () => {
 
   const [people, setPeople] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchPeople = async() => {
-      const response = await axios.get('http://api.open-notify.org/astros.json')
-      console.log("Data: ", response.data);
-      setPeople(response.data.people)
+      try {
+        const response = await axios.get('http://api.open-notify.org/astros.json');
+        console.log('response: ', response);
+        setPeople(response.data.people)
+        setIsLoading(false)
+      } catch (error) {
+        console.error("Something went wrong!");
+        setIsLoading(true)
+      }
     }
     fetchPeople();
   },[])
@@ -20,10 +28,11 @@ const PeopleInSpace = () => {
   return (
   <div className="container">
     <div className="astronauts"></div>
+    <SyncLoader color="#1e2739" loading={isLoading} size={50} />
     <div className="peopleInSpace">
-      {people.map((person, idx) => (
+      {people?.map((person, idx) => (
         <a key={person.name} className="names" href={`https://en.wikipedia.org/wiki/${person.name}`}
-        target={"_blank"} rel="noreferrer">{person.name}</a>
+        target={"_blank"} rel="noreferrer">{person.name}<p>Currently on the {person.craft}</p></a>
       ))}
     </div>
   </div>
