10c02c3b7c0adf099d23b6b6184b98aa08cf20b3
mtbishop <bishopmatthew76@gmail.com>
2022-01-07 11:46:59 -0700

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./styles/App.scss";
+import { Route, Routes } from "react-router-dom"
+import HomePage from './pages/HomePage'
 
 function App() {
-  return <div>APP</div>;
+  return (
+  <div className="container">
+    <Routes>
+      <Route path='/' element={<HomePage />}/>
+    </Routes>
+  </div>
+  );
 }
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
-  return <div>HOMEPAGE</div>;
+  return(
+    <div className="container homepage">
+       <div className="card">
+         <div className="people" />
+          <Link to="/people">
+          People in Space
+          </Link>
+       </div>
+       <div className="card">
+         <div className="tracker" />
+          <Link to="/tracker">
+          Tracker
+          </Link>
+       </div>
+    </div>
+  );
 };
 
 export default HomePage;
